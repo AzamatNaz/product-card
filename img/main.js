796916061b99d210fb7c73da033cddd0cd61b9f5
azamat <solievazamat2001@gmail.com>
2026-08-14 13:10:05 +0300

    gjg

---
+++ img/main.js
@@ -0,0 +1,21 @@
+const firstCardButton = document.querySelector("#change-color-first-card");
+
+firstCardButton.addEventListener("click", function () {
+    const firstCard = document.querySelector(".product-card");
+    firstCard.style.backgroundColor = "lightgreen";
+});
+const allCardsButton = document.querySelector("#change-color-all-cards");
+
+allCardsButton.addEventListener("click", function () {
+    const cards = document.querySelectorAll(".product-card");
+
+    cards.forEach(function (card) {
+        card.style.backgroundColor = "lightgreen";
+    });
+});
+
+const googleButton = document.querySelector("#open-google-button");
+
+googleButton.addEventListener("click", function () {
+    window.open("https://google.com");
+});
